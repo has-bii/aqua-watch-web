c18a55a4a802a87db02d3c4f40e42fd2c571d87e
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2024-12-18 00:37:08 +0300

    feat: logout server action

---
+++ src/app/auth/actions.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export const logout = async () => {
+  const supabase = await createClient();
+
+  // Check if a user's logged in
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    await supabase.auth.signOut();
+  }
+
+  revalidatePath("/", "layout");
+  return redirect("/auth/sign-in");
+};
