5b1a1d855a550dff2edb8f80e9b0664f779e06a0
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2024-12-18 00:35:12 +0300

    feat: react query devtools

--- src/app/providers/root-provider.tsx
+++ src/app/providers/root-provider.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { Toaster } from "sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 type Props = {
   children: React.ReactNode;
@@ -15,6 +16,7 @@ export default function RootProvider({ children }: Props) {
     <QueryClientProvider client={queryClient}>
       {children}
       <Toaster theme="light" richColors />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
