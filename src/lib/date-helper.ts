15a22b73f6d35d41cb7dcd0bc465b61e6252bb40
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2024-12-29 00:18:34 +0300

    feat: hour helper

--- src/lib/date-helper.ts
+++ src/lib/date-helper.ts
@@ -1,3 +1,8 @@
 import { format } from "date-fns";
 
 export const dateFormat = (date: Date | string): string => format(date, "PPP");
+
+export const hourFormat = (
+  date: Date | string,
+  formatString?: string,
+): string => format(date, formatString ?? "k");
