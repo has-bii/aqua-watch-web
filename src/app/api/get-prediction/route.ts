fb0979498f0b80230f0ab2b8ca388246bd729f9c
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2025-01-04 02:20:05 +0300

    feat: get prediction

---
+++ src/app/api/get-prediction/route.ts
@@ -0,0 +1,51 @@
+import { type NextRequest } from "next/server";
+import { z } from "zod";
+
+const schema = z.object({
+  env_id: z.string().uuid(),
+});
+
+export async function GET(request: NextRequest) {
+  try {
+    const searchParams = request.nextUrl.searchParams;
+    const env_id = searchParams.get("env_id");
+
+    const { error, data: parsed_data } = schema.safeParse({
+      env_id,
+    });
+
+    if (error) throw new ReferenceError("Env ID and datetime are required!");
+
+    const res = await fetch(
+      `${process.env.FASTAPI_URL}/predict/v2?env_id=${parsed_data.env_id}`,
+      {
+        next: {
+          revalidate: 300,
+        },
+      },
+    );
+
+    if (!res.ok) throw new Error();
+
+    const data = await res.json();
+
+    return Response.json(data);
+  } catch (error) {
+    console.log(error);
+
+    if (error instanceof ReferenceError)
+      return Response.json(
+        {
+          message: error.message,
+        },
+        { status: 404 },
+      );
+
+    return Response.json(
+      {
+        message: "Unexpected error has occurred!",
+      },
+      { status: 500 },
+    );
+  }
+}
---
+++ src/hooks/prediction/index.ts
@@ -0,0 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+type TPrediction = {
+  data: {
+    predictions: { timestamp: string; temperature: number }[];
+    averages: { time: number; avg_temp: number }[];
+    accuracy: {
+      mse: number;
+      r2: number;
+    };
+  };
+};
+
+export const useGetPrediction = (env_id: string) =>
+  useQuery({
+    queryKey: ["prediction", env_id],
+    refetchOnWindowFocus: false,
+    queryFn: async () => {
+      try {
+        const res = await fetch(`/api/get-prediction?env_id=${env_id}`);
+
+        if (!res.ok) throw new Error();
+
+        return (await res.json()) as TPrediction;
+
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      } catch (error) {
+        toast.error("An error occurred while getting predictions");
+      }
+    },
+  });
